03f5f22719a1dd253c22570a70b73492ac56bd42
TitouanSans <Titouan.sanshochart@gmail.com>
2020-12-21 16:50:41 +0100

    try get data

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const API_key = '9301dbfb9eeb49ae99cfbdd92f8bcc47';
 
-let url = 'http://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          API_key;
-let req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+let url_bbc = 'http://newsapi.org/v2/top-headlines?' +
+'sources=bbc-news&' + API_key;
+
+let req = new Request(url_bbc);
+fetch(req).then(function(response) {
+    console.log(response.json());
+    const news = response;
+    console.log(news);
+})
